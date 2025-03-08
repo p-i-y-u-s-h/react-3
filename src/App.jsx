f63e201d112c0e8c07c0d30a7ad6dc4a50a2c48f
Piyush Mahajan <mahajanpiyush005@gmail.com>
2025-03-09 02:39:33 +0530

    commit-5

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,41 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div style={{ display: "flex", flexDirection: "column", height: "100vh" }}>
+      <div style={{ background: "#a3a0a9", width: "100%", height: 75, display: "flex", alignItems: "center", justifyContent: "center" }}>
+        <NavBarComponent />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+
+      <div style={{ display: "flex", flex: 1 }}>
+        <div style={{ background: "#c8dceb", flex: 1, display: "flex", alignItems: "center", justifyContent: "center" }}>
+          <ProfileComponent />
+        </div>
+
+        <div style={{ background: "#ede99d", flex: 2, display: "flex", alignItems: "center", justifyContent: "center" }}>
+          <PostComponent />
+        </div>
+
+        <div style={{ background: "#ecd9dd", flex: 1, display: "flex", alignItems: "center", justifyContent: "center" }}>
+          <SettingsComponent />
+        </div>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+function NavBarComponent() {
+  return <div style={{ fontWeight: "bold" }}>This is Navbar</div>;
+}
+
+function PostComponent() {
+  return <div>This is posts area</div>;
+}
+
+function ProfileComponent() {
+  return <div>This is profile area</div>;
+}
+
+function SettingsComponent() {
+  return <div>This is settings area</div>;
+}
+
+export default App;
